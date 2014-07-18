a6231826123bbfba56d124b2c5077806f208dd21
kmcguire <kmcg3413@gmail.com>
2014-07-17 22:14:39 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import stageone
+
+def level2check(r):
+    if r == 'ef' or r == b'ef':
+        return True
+    return False
+
+def level1check(r):
+    if r != 6:
+        return False
+    return True
+
+def truecheck(r):
+    return r
+
+def main():
+    levels = (
+        #(stageone.level1, level1check),
+        #(stageone.level2, level2check),
+        #(stageone.level3, truecheck),
+        (stageone.level4, truecheck),
+    )
+
+
+    for x in range(0, len(levels)):
+        try:
+            if not levels[x][1](levels[x][0]()):
+                print('LEVEL %s FAILED BECAUSE OF RESULT' % (x + 1))
+                return
+        except Exception as e:
+            print('LEVEL %s FAILED BECAUSE OF EXCEPTION' % (x + 1))
+            raise e
+        print('LEVEL %s PASSED' % (x + 1))
+
+
+    return
+
+main()
---
+++ stageone.py
@@ -0,0 +1,39 @@
+'''
+    The result should be 6.
+'''
+def level1():
+    x = 3
+    return x + "3"
+
+'''
+    We need to create either a string or bytes object.
+'''
+def level2():
+    return b'e' + 'f'
+
+'''
+    Why does X never equal 100?
+'''
+def level3():
+    for x in range(0, 100):
+        print(x)
+    if x == 100:
+        return True
+    return False
+
+'''
+    Why is this never true?
+'''
+def level4():
+    v = 0.1 + .02
+    return v == .12
+
+'''
+    
+'''
+
+
+
+
+
+# types used incorrectly
