74e136bff7cb2d58f95d9aab345ab5100619f3e5
kmcguire <kmcg3413@gmail.com>
2014-07-17 23:13:00 -0500

    added more bugs

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import stageone
 
-def level2check(r):
+def level2check(r, _):
     if r == 'ef' or r == b'ef':
         return True
     return False
 
-def level1check(r):
+def level1check(r, _):
     if r != 6:
         return False
     return True
 
-def truecheck(r):
+def retcheck(r, _):
     return r
 
 def main():
     levels = (
-        (stageone.level1, level1check),
-        (stageone.level2, level2check),
-        (stageone.level3, truecheck),
-        (stageone.level4, truecheck),
+        (stageone.level_notgoodtypes, level1check, None),
+        (stageone.level_tryconcating, level2check, None),
+        (stageone.level_mayberangefellshort, retcheck, True),
+        (stageone.level_floatingnumbersnotright, retcheck, True),
+        (stageone.level_indexingstringkindaconfusing, retcheck, 'h'),
+        (stageone.level_shiftingisweird, retcheck, 0x80),
+        (stageone.level_shiftingisweirder, retcheck, 0x01),
+        (stageone.level_andingforabit, retcheck, 0x88),
+        (stageone.level_bytesbutstrsame, retcheck, True),
+        (stageone.level_whydifferentencoding, retcheck, True),
+        (stageone.level_andsomestrings, retcheck, 'ab'),
     )
 
 
     for x in range(0, len(levels)):
         try:
-            if not levels[x][1](levels[x][0]()):
+            if not levels[x][1](levels[x][0](), levels[x][2]):
                 print('LEVEL %s FAILED BECAUSE OF RESULT' % (x + 1))
                 return
         except Exception as e:
--- stageone.py
+++ stageone.py
@@ -1,39 +1,107 @@
 '''
     The result should be 6.
 '''
-def level1():
+def level_notgoodtypes():
     x = 3
     return x + "3"
 
 '''
     We need to create either a string or bytes object.
 '''
-def level2():
+def level_tryconcating():
     return b'e' + 'f'
 
 '''
     Why does X never equal 100?
 '''
-def level3():
+def level_mayberangefellshort():
     for x in range(0, 100):
-        print(x)
+        pass
     if x == 100:
         return True
     return False
 
 '''
-    Why is this never true?
+    Why is this never true? And, can you fix it?
 '''
-def level4():
+def level_floatingnumbersnotright():
     v = 0.1 + .02
     return v == .12
 
 '''
-    
+    We should return the first letter of the string,
+    but do we? The first letter is `h`. Why is it
+    not returning `h`.
 '''
+def level_indexingstringkindaconfusing():
+    s = 'hello world'
+    return s[1]
 
+'''
+    We are trying to set the 8th bit of the
+    integer to one.
 
+    This function should return 128 or 0x80.
+'''
+def level_shiftingisweird():
+    return 1 << 8
 
+'''
+    This function should return one by doing
+    a shifting operation. We need to shift
+    the 8th bit to the 1st bit.
+'''
+def level_shiftingisweirder():
+    return 0x80 >> 8
 
+'''
+    Can you spot the problem? We should return a
+    the value 0x88 after the operation. You can
+    easily just modify it to return 0x88, but 
+    that would be cheating.
+'''
+def level_andingforabit():
+    return 0x88 & (0x11 << 7)
 
-# types used incorrectly
+'''
+    Can you make them equal? In both ways...
+
+    Hint.. ord(x) or chr(x)
+    Hint.. bytes or decode
+'''
+def level_bytesbutstrsame():
+    # an example
+    a = b'hello world'
+    b = b.decode('utf8')                # treats bytes as if they are in the UTF8 encoding
+    c = chr(65)
+    d = ord('A')
+    e = bytes('hello world', 'utf8')    # converts unicode string into UTF8 encoding
+    print(a, b, c, d)
+
+    # the problem
+    b = b'hello world'
+    s = 'hello world'
+    return b[0] == s[0] and b == s
+
+'''
+    Why do these not equal each other? Can it
+    be fixed? How do we fix the problem and why
+    did we have to fix it?
+'''
+def level_whydifferentencoding():
+    z = 'hello world'
+    a = bytes(z, 'utf8')
+    b = bytes(z, 'utf16')
+
+    r1 = a[0] + 146 == b
+    r2 = a[0] == b[0]
+
+    return r1 and r2 
+
+'''
+    We need to combine the strings to produce
+    the string `ab` why does it not work?
+'''
+def level_andsomestrings():
+    return 'a' and 'b'
+
